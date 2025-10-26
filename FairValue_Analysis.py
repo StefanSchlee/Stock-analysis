069292a57e77fb1b7d00793be4b2c5561b46c933
Stefan Schlee <stefan.schlee99@gmail.com>
2025-10-26 16:05:34 +0100

    add old state

---
+++ FairValue_Analysis.py
@@ -0,0 +1,182 @@
+#tested with python 3.11
+
+from yahooquery import Ticker
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')  # or 'Qt5Agg'
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline, pchip_interpolate
+
+
+###########################
+# symbol = 'MC.PA'
+# symbol = 'PAYC'
+# symbol = 'PYPL'
+symbol = 'AAPL'
+# symbol = 'ADBE'
+# symbol = 'NFLX'
+# symbol = '7CD.BE'
+# symbol = 'MNST'
+# symbol = 'MSFT'
+# symbol = 'AMZN'
+# symbol = 'SBUX'
+# symbol = 'PUP'
+# symbol = 'AMD'
+###########################
+
+print(f"Starting Analysis for {symbol} ...")
+
+ticker = Ticker(symbol)
+
+earningsTrend = ticker.earnings_trend
+earningsTrend = earningsTrend[symbol]['trend']
+
+# print(len(earningsTrend))
+# print(earningsTrend)
+
+#extract estimate of current year
+currentYearEarningEstimateDict = None
+nextYearEarningsEstimateDict = None
+for period in earningsTrend:
+    if period['period'] == "0y":
+        currentYearEarningEstimateDict = period
+    if period['period'] == "+1y":
+        nextYearEarningsEstimateDict = period
+
+print(f"Last year eps estimate: {currentYearEarningEstimateDict['earningsEstimate']['yearAgoEps']}")
+print(f"Current year eps estimate: {currentYearEarningEstimateDict['earningsEstimate']['avg']}")
+print(f"Next year eps estimate: {nextYearEarningsEstimateDict['earningsEstimate']['avg']}")
+
+
+#extract eps of last four years
+lastIncome = ticker.income_statement(trailing=False)
+dates = lastIncome['asOfDate']
+years = dates.dt.year
+lasteps = lastIncome['BasicEPS']
+
+fig, ax = plt.subplots()
+ax.bar(dates.array.date, lasteps, width=pd.Timedelta(days=30), label="Historic EPS")
+ax.bar(pd.Timestamp(currentYearEarningEstimateDict['endDate']), currentYearEarningEstimateDict['earningsEstimate']['avg'], width=pd.Timedelta(days=30), label="Estimate +0y")
+ax.bar(pd.Timestamp(nextYearEarningsEstimateDict['endDate']), nextYearEarningsEstimateDict['earningsEstimate']['avg'], width=pd.Timedelta(days=30), label='Estimate +1y')
+ax.bar(dates.array.date[-1], currentYearEarningEstimateDict['earningsEstimate']['yearAgoEps'], width=pd.Timedelta(days=10), label="Estimate -1y")
+ax.legend()
+ax.set_title("Gewinn pro Aktie (Raw Data)")
+ax.grid()
+
+#extract cashflow of last years, create Cashflow series
+LastCashFlow = ticker.cash_flow()
+CashFlowDates = LastCashFlow['asOfDate']
+OperativeCashFlow = LastCashFlow['CashFlowFromContinuingOperatingActivities']
+CashFlowSeries = pd.Series(OperativeCashFlow.array, CashFlowDates)
+
+fig, ax = plt.subplots()
+ax.bar(CashFlowSeries.index, OperativeCashFlow.array, width=pd.Timedelta(days=30), label="Historic Operative Cashflow")
+ax.legend()
+ax.set_title("Cash Flow")
+ax.grid()
+
+
+#eps estimate correction (divergence between eps calculation of estimate and history, see GAAP, non-GAAP)
+EPScorrectionFactor = lasteps[-1] / currentYearEarningEstimateDict['earningsEstimate']['yearAgoEps']
+print(f"EPS Correction factor: {EPScorrectionFactor}")
+
+#create eps series
+eps_array = lasteps.to_numpy()
+eps_array = np.append(eps_array, currentYearEarningEstimateDict['earningsEstimate']['avg'] * EPScorrectionFactor)
+eps_array = np.append(eps_array, nextYearEarningsEstimateDict['earningsEstimate']['avg'] * EPScorrectionFactor)
+
+eps_dates_array = dates.to_numpy()
+eps_dates_array= np.append(eps_dates_array,np.datetime64(currentYearEarningEstimateDict['endDate']))
+eps_dates_array= np.append(eps_dates_array,np.datetime64(nextYearEarningsEstimateDict['endDate']))
+
+eps_series = pd.Series(eps_array,pd.DatetimeIndex(eps_dates_array))
+
+fig, ax = plt.subplots()
+ax.bar(eps_series.index, eps_series.array , width=pd.Timedelta(days=30))
+ax.grid()
+ax.set_title("EPS corrected")
+
+#get chart history
+chartHistory = ticker.history(period='5y',interval='1d',adj_timezone=False)['close'][symbol]
+chartHistory.index = pd.DatetimeIndex(chartHistory.index).tz_localize(None)
+
+fig, ax = plt.subplots()
+ax.plot(chartHistory)
+ax.grid()
+ax.set_title("Chart 5y")
+
+# current KGV
+KGV = chartHistory.array[-1] / eps_series.truncate(after=chartHistory.index[-1]).array[-1]
+KGVe = chartHistory.array[-1] / eps_series.truncate(before=chartHistory.index[-1]).array[0]
+print(f"KGV: {KGV}")
+print(f"KGVe: {KGVe}")
+
+#live KGV
+oldestValidKGVDate = eps_series.index[0] - pd.Timedelta(days=365)
+liveKGV = pd.Series(np.nan, chartHistory.truncate(before=oldestValidKGVDate).index)
+for earningsDate in eps_series.index[::-1]:
+    earlierDates = liveKGV.truncate(after=earningsDate.date()).index
+    liveKGV[earlierDates] = chartHistory[earlierDates] / eps_series[earningsDate]
+
+#for calculation of fair value, lower bound KGV to zero in case the EPS were negative
+liveKGVBounded = liveKGV.clip(lower=0)
+
+fig, ax = plt.subplots()
+ax.plot(liveKGV)
+ax.plot(liveKGVBounded, '--', label="bounded")
+ax.grid()
+ax.set_title("KGVe")
+for date in eps_series.index:
+    ax.axvline(date,ls='--',c='k')
+
+# live KCV
+oldestValidKCVDate = CashFlowSeries.index[0] - pd.Timedelta(days=365)
+liveKCV = pd.Series(np.nan, chartHistory.truncate(before=oldestValidKCVDate).index)
+for date in CashFlowSeries.index[::-1]:
+    earlierDates = liveKCV.truncate(after=earningsDate.date()).index
+    liveKCV[earlierDates] = chartHistory[earlierDates] / CashFlowSeries[date]
+
+fig, ax = plt.subplots()
+ax.plot(liveKCV)
+ax.set_title("KCVe")
+for date in CashFlowSeries.index:
+    ax.axvline(date,ls='--',c='k')
+
+#fair value at earnings release (doku in eulerpool pinned post)
+movingAverageTime = pd.Timedelta(days=365*3)
+fairValue_value = []
+
+for earningsDate in eps_series.index:
+    averagingStartDate = (earningsDate-movingAverageTime)
+
+    #average over all data availabe, but maximal over <movingAverageTime>
+    meanKGV = liveKGVBounded.truncate(before=averagingStartDate,after=earningsDate).array.mean()
+    fairValue_value.append(meanKGV*eps_series[earningsDate])
+fairValue = pd.Series(fairValue_value, eps_series.index)
+
+#interpolate fair value
+# fairValueSpline = CubicSpline(fairValue.index.values.astype('float'),fairValue.array, bc_type='natural')
+# fairValueDateFine = np.linspace(fairValue.index.values[0].astype('float'),fairValue.index.values[-1].astype('float'),1000)
+# fairValueFine = fairValueSpline(fairValueDateFine)
+
+fairValueDateFine = np.linspace(fairValue.index.values[0].astype('float'),fairValue.index.values[-1].astype('float'),1000)
+fairValueFine = pchip_interpolate(fairValue.index.values.astype('float'),fairValue.array, fairValueDateFine)
+
+
+
+fig, ax = plt.subplots()
+ax.plot(chartHistory, label="Kurs")
+ax.grid()
+
+# ax.plot(fairValue)
+ax.plot(fairValue.index, fairValue.array, ls='', marker='x', label="Fair value",c='k')
+ax.plot(pd.to_datetime(fairValueDateFine), fairValueFine, label=None, c='k')
+# +-20% fair Value
+ax.plot(pd.to_datetime(fairValueDateFine), 1.2*fairValueFine, label="+20%", c='r')
+ax.plot(pd.to_datetime(fairValueDateFine), 0.8*fairValueFine, label="-20%", c='g')
+
+ax.legend()
+
+plt.show() 
+pass
---
+++ RelativeChartComparison.py
@@ -0,0 +1,40 @@
+# Task: Compare two stocks
+
+from yahooquery import Ticker
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+###########################
+# symbol1 = 'MC.PA'
+symbol1 = 'SBUX'
+symbol2 = 'PSA'
+###########################
+
+ticker1 = Ticker(symbol1)
+ticker2 = Ticker(symbol2)
+
+#get chart
+chartHistory1 = ticker1.history(period='5y',interval='1d',adj_timezone=False)['close'][symbol1]
+chartHistory1.index = pd.DatetimeIndex(chartHistory1.index).tz_localize(None)
+chartHistory2 = ticker2.history(period='5y',interval='1d',adj_timezone=False)['close'][symbol2]
+chartHistory2.index = pd.DatetimeIndex(chartHistory2.index).tz_localize(None)
+
+
+#calculate relative chart
+relChartHistory1 = pd.Series(chartHistory1.array/chartHistory1.array[0]*100,chartHistory1.index) 
+relChartHistory2 = pd.Series(chartHistory2.array/chartHistory2.array[0]*100,chartHistory2.index) 
+
+#plot rel chart history
+fig, ax = plt.subplots()
+ax.plot(relChartHistory1, label=symbol1)
+ax.plot(relChartHistory2, label=symbol2)
+
+ax.grid()
+ax.set_title("Relative Chart")
+ax.legend()
+
+
+
+
+plt.show()
